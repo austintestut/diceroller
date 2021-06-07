aeca23031da1280bb75e16f2ac15768a512d9582
Austin Testut <austintestut@gmail.com>
2021-06-07 14:32:11 -0500

    first commit

---
+++ Dice.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import d20 from './assets/d20.png';
+
+const Dice = () => {
+return (
+  <div>
+    <img src={d20} />
+  </div>
+)
+};
+
+
+
+export default Dice;

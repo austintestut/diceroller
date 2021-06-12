8b89c20213ef231b93d27b2c58880d25b41f2843
Austin Testut <austintestut@gmail.com>
2021-06-12 15:21:53 -0500

    added file to use for variable Image sources

---
+++ assets/assets.js
@@ -0,0 +1,11 @@
+const images = {
+  d4: require('./d4.png'),
+d6: require('./d6.png'),
+d8: require('./d8.png'),
+d10: require('./d10.png'),
+percent: require('./percent.png'),
+d12: require('./d12.png'),
+d20: require('./d20.png'),
+};
+
+export default images;
